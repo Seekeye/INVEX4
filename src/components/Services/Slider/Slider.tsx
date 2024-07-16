360f15d380a73bd4529a9c0d57aba70e90156ea1
Seekeye <166416561+Seekeye@users.noreply.github.com>
2024-07-17 01:46:14 +0200

    Update Slider.tsx

--- src/components/Services/Slider/Slider.tsx
+++ src/components/Services/Slider/Slider.tsx
@@ -1,9 +1,9 @@
-import { Navigation, Scrollbar } from "swiper/modules";
+import { Scrollbar, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css/autoplay"; // Importar CSS de autoplay
 import { sliderDataImages } from "./Slider.data";
 import Image from "next/image";
 
@@ -13,10 +13,15 @@ export function Slider() {
       slidesPerView={2.5}
       spaceBetween={15}
       freeMode={true}
-      navigation
-      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 3000, // Tiempo de retraso en milisegundos entre transiciones automáticas
+        disableOnInteraction: false, // No deshabilitar autoplay en la interacción del usuario
+        pauseOnMouseEnter: true, // Pausar autoplay cuando el mouse está sobre el slider
+      }}
+      loop={true} // Habilitar loop para que el slider pase continuamente
+      speed={2000}
       scrollbar={{ draggable: true }}
-      modules={[Navigation, Scrollbar]}
+      modules={[Scrollbar, Autoplay]}
       className="h-[200px] md:h-[320px] w-[300px] md:w-[500px]"
       grabCursor
     >
