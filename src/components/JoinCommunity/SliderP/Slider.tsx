336bd4869f6f61c8062e899a11d6dddce3394534
Seekeye <166416561+Seekeye@users.noreply.github.com>
2024-07-17 00:43:38 +0200

    Update Slider.tsx

--- src/components/JoinCommunity/SliderP/Slider.tsx
+++ src/components/JoinCommunity/SliderP/Slider.tsx
@@ -10,34 +10,48 @@ import Image from "next/image";
 
 export function Slider() {
   return (
-    <div className="w-full h-80">
-      {" "}
+    <div className="w-full h-60 sm:h-80 md:h-96 lg:h-112">
       <Swiper
-        slidesPerView={4}
-        spaceBetween={15}
+        slidesPerView={2}
+        spaceBetween={10}
         freeMode={true}
         autoplay={{
           delay: 300,
           disableOnInteraction: false,
           pauseOnMouseEnter: true,
-        }} // Configuración de autoplay
-        loop={true} // Habilitar loop
+        }}
+        loop={true}
         speed={2000}
         modules={[Navigation, Scrollbar, Autoplay]}
         className="h-full w-full"
         grabCursor
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+        }}
       >
         {sliderDataImages.map(({ id, urlImage }) => (
           <SwiperSlide key={id} className="flex justify-center items-center">
             <div className="w-full flex justify-center items-center">
-              <Image
-                src={`/assets/houses/${urlImage}`}
-                alt="House"
-                width={300}
-                height={350}
-                className="rounded-xl"
-                objectFit="contain"
-              />
+              <div className="w-48 h-48 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 flex justify-center items-center">
+                <Image
+                  src={`/assets/houses/${urlImage}`}
+                  alt="House"
+                  width={300}
+                  height={350}
+                  className="rounded-xl object-cover"
+                />
+              </div>
             </div>
           </SwiperSlide>
         ))}
